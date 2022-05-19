5114294536b61e9973d80c86ce6c57b2a4c4f6ee
Katie Anderson <katherine.lizf@gmail.com>
2022-05-18 19:46:49 -0500

    php email form lib

--- forms/contact.php
+++ forms/contact.php
@@ -6,7 +6,6 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'katie.fullstackwebdev@gmail.com';
 
   if( file_exists($php_email_form = '../vendor/php-email-form/php-email-form.php' )) {
@@ -22,16 +21,6 @@
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
